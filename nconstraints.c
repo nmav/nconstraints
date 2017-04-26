b9c2a563e2e7e14ef3024ca68357d19613215b60
Kai Engert <kaie@kuix.de>
2017-04-26 17:25:56 +0200

    Write the binary extension to a file, to allow easier verification of the created result with dumpasn1 tools etc.

--- nconstraints.c
+++ nconstraints.c
@@ -36,10 +36,21 @@ static void create_ext(const char *oid, const gnutls_datum_t *data, gnutls_datum
 	return;
 }
 
+void write_to_file(const gnutls_datum_t *data, const char *filename)
+{
+	unsigned i;
+	FILE *fp = fopen(filename, "wb");
+	assert(fp != NULL);
+	fwrite(data->data, data->size, 1, fp);
+	fclose(fp);
+}
+
 void funny_print(const gnutls_datum_t *data)
 {
 	unsigned i;
 
+	printf("String that represents the domain constraints extension in p11-kit"
+		   "file format:\n");
 	for (i=0;i<data->size;i++) {
 		printf("%%%.2x", (unsigned)data->data[i]);
 	}
@@ -51,6 +62,9 @@ int main(int argc, char **argv)
 	gnutls_x509_name_constraints_t nc;
 	gnutls_datum_t der, out;
 	unsigned i;
+	const char *filename_prefix = "constraints-extension-for:";
+	unsigned filename_len = strlen(filename_prefix);
+	char *filename;
 
 	if (argc < 2) {
 		printf("usage: %s [domain1] [domain2] ...\n", argv[0]);
@@ -65,7 +79,20 @@ int main(int argc, char **argv)
 		assert(gnutls_x509_name_constraints_add_permitted(nc,
 							   GNUTLS_SAN_DNSNAME,
 							   &name) >= 0);
+		filename_len += name.size + 1;
 	}
+
+	filename = malloc(filename_len);
+	assert(filename != NULL);
+	strcpy(filename, filename_prefix);
+
+	for (i=0;i<argc-1;i++) {
+		strcat(filename, argv[i+1]);
+		if (i < argc-2) {
+			strcat(filename, "_");
+		}
+	}
+	printf("writing binary extension to file: %s\n", filename);
 
 	assert(gnutls_x509_ext_export_name_constraints(nc, &der) >= 0);
 	gnutls_x509_name_constraints_deinit(nc);
@@ -74,5 +101,6 @@ int main(int argc, char **argv)
 	gnutls_free(der.data);
 
 	funny_print(&out);
+	write_to_file(&out, filename);
 	free(out.data);
 }
